af40b9cffe00bf84076e52eac87ec467a224aa56
N1ghtStorm <bushuevdv@mail.ru>
2021-04-14 15:34:50 +0300

    add csharp

---
+++ csharptest/csharptest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw1 = new Stopwatch();
+            sw1.Start();
+            some_1(0);
+            sw1.Stop();
+            Console.WriteLine(sw1.ElapsedTicks);
+
+            var sw2 = new Stopwatch();
+            sw2.Start();
+            var a = 0;
+            some_2(ref a);
+            sw2.Stop();
+            Console.WriteLine(sw2.ElapsedTicks);
+        }
+
+
+        static void some_1(int a)
+        {
+            if (a >= 15000)
+            {
+                return;
+            }
+
+            a++;
+            some_1(a);
+        }
+
+        static void some_2(ref int a)
+        {
+            if (a >= 15000)
+            {
+                return;
+            }
+
+            a++;
+            some_2(ref a);
+        }
+    }
+}
